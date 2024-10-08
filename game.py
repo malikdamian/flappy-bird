2f60c5aff10f2df16addde89230869e13a533612
Damian Malik <malikdamiann@gmail.com>
2024-10-08 22:22:16 +0200

    create game indicator class

---
+++ game.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame import Surface
+
+from settings import WIDTH
+
+pygame.font.init()
+
+
+class GameIndicator:
+    def __init__(self, screen: Surface) -> None:
+        self.screen = screen
+        self.font = pygame.font.SysFont("Bauhaus 93", 60)
+        self.inst_font = pygame.font.SysFont("Bauhaus", 30)
+        self.color = pygame.Color("white")
+        self.inst_color = pygame.Color("black")
+
+    def show_score(self, int_score) -> None:
+        bird_score = str(int_score)
+        score = self.font.render(bird_score, True, self.color)
+        self.screen.blit(score, (WIDTH // 2, 50))
+
+    def instructions(self) -> None:
+        inst_text1 = "Press SPACE button to Jump"
+        inst_text2 = "Press \"R\" Button to Restart Game"
+        ins1 = self.inst_font.render(inst_text1, True, self.inst_color)
+        ins2 = self.inst_font.render(inst_text2, True, self.inst_color)
+        self.screen.blit(ins1, (95, 400))
+        self.screen.blit(ins2, (70, 450))
